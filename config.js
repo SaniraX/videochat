cbdcb0d98cac83c694cb37e9a1a2213a82d56c3e
sanirachathuranga <sanirachathuranga@gmail.com>
2016-04-18 14:42:21 +0530

    quickbox.js

--- config.js
+++ config.js
@@ -40,6 +40,129 @@
 
   var QBUsersQA = [
     {
+      id: 5395743,
+      login: 'webuser111',
+      password: 'webuser111',
+      full_name: 'User 1',
+      colour: 'FD8209'
+    },
+    {
+      id: 5395747,
+      login: 'webuser112',
+      password: 'webuser112',
+      full_name: 'User 2',
+      colour: '11a209'
+    },
+    {
+      id: 5681538,
+      login: 'webuser113',
+      password: 'webuser113',
+      full_name: 'User 3',
+      colour: '11a2a9'
+    },
+    {
+      id: 5719859,
+      login: 'webuser114',
+      password: 'webuser114',
+      full_name: 'User 4',
+      colour: '51c209'
+    },
+    {
+      id: 5719860,
+      login: 'webuser115',
+      password: 'webuser115',
+      full_name: 'User 5',
+      colour: '511209'
+    },
+    {
+      id: 5719866,
+      login: 'webuser116',
+      password: 'webuser116',
+      full_name: 'User 6',
+      colour: '01e209'
+    }
+  ],
+  QBUsersDev = [
+    {
+      id: 6970356,
+      login: 'dev_user_1',
+      password: 'dev_user_1',
+      full_name: 'User 1',
+      colour: 'ffaa00'
+    },
+    {
+      id: 6970368,
+      login: 'dev_user_2',
+      password: 'dev_user_2',
+      full_name: 'User 2',
+      colour: '0890ff'
+    },
+    {
+      id: 6970375,
+      login: 'dev_user_3',
+      password: 'dev_user_3',
+      full_name: 'User 3',
+      colour: 'ff03a6'
+    },
+    {
+      id: 6970379,
+      login: 'dev_user_4',
+      password: 'dev_user_4',
+      full_name: 'User 4',
+      colour: '60e27a'
+    }
+  ],
+  QBUsersProd = [
+    {
+      id: 2436251,
+      login: 'webrtc_user1',
+      password: 'x6Bt0VDy5',
+      full_name: 'User 1',
+      colour: 'FD8209'
+    },
+    {
+      id: 2436254,
+      login: 'webrtc_user2',
+      password: 'x6Bt0VDy5',
+      full_name: 'User 2',
+      colour: '1765FB'
+    },
+    {
+      id: 2436257,
+      login: 'webrtc_user3',
+      password: 'x6Bt0VDy5',
+      full_name: 'User 3',
+      colour: 'F81480'
+    },
+    {
+      id: 2436258,
+      login: 'webrtc_user4',
+      password: 'x6Bt0VDy5',
+      full_name: 'User 4',
+      colour: '39A345'
+    },
+    {
+      id: 2436259,
+      login: 'webrtc_user5',
+      password: 'x6Bt0VDy5',
+      full_name: 'User 5',
+      colour: '921392'
+    },
+    {
+      id: 2436262,
+      login: 'webrtc_user6',
+      password: 'x6Bt0VDy5',
+      full_name: 'User 6',
+      colour: '6594C5'
+    },
+    {
+      id: 2436263,
+      login: 'webrtc_user7',
+      password: 'x6Bt0VDy5',
+      full_name: 'User 7',
+      colour: 'C1061E'
+    },
+    {
       id: 2436265,
       login: 'webrtc_user8',
       password: 'x6Bt0VDy5',
